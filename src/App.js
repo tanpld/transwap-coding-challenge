52b4c39975c16488cf062d0ba4ff053fa25b3a34
Tan Tan <tantan@nagagames.com>
2021-08-15 11:34:06 +0700

    feat: Bank component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Country from './components/Country';
+import Bank from './components/Bank';
 function App() {
   return (
     <div className="App" style={{ backgroundColor: '#F2F3FA'}}>
       <Country />
+      <Bank />
     </div>
   );
 }
